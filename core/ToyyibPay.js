3028841a4701b5a8508b1d3f78c5326e684f5851
Fadhil Ahmad <fadzbobby95@gmail.com>
2020-03-08 22:49:41 +0800

    feat: initiat class base function

---
+++ core/ToyyibPay.js
@@ -0,0 +1,22 @@
+class ToyyibPay {
+  constructor(secret, { dev } = {}) {
+    this.secret = secret;
+    this.isDev = dev || false;
+  }
+  getLibrary() {}
+  getBank() {}
+  getBankFPX() {}
+  getPackage() {}
+  createAccount() {}
+  getUserStatus() {}
+  getAllUser() {}
+  createCategory() {}
+  getCategoryDetails() {}
+  createBill() {}
+  createBillMultiPayment() {}
+  runBill() {}
+  getAllBill() {}
+  getBillTransactions() {}
+  getSettlement() {}
+  getSettlementSummary() {}
+}
